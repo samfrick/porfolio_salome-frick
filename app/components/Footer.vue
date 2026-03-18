af2dfa94f18810a2466011cb73d776012f1731e5
Salomé Frick <salome.frick@free.fr>
2026-03-18 20:13:06 +0100

    update footer

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -1,1 +1,19 @@
-<template></template>
+<script setup lang="ts">
+</script>
+
+<template>
+    <UFooter class="border-t border-accented mt-10">
+        <template #left>
+            <p class="text-muted text-sm">Copyright © {{ new Date().getFullYear() }} - Salomé Frick</p>
+        </template>
+
+        <template #right>
+            <UButton icon="i-simple-icons-linkedin" color="neutral" variant="ghost"
+                to="https://www.linkedin.com/in/salomé-frick-636034294" target="_blank" aria-label="Discord" />
+            <UButton icon="i-simple-icons-github" color="neutral" variant="ghost" to="https://github.com/samfrick"
+                target="_blank" aria-label="GitHub" />
+            <UButton icon="lucide:mail" color="neutral" variant="ghost" to="mailto:salome.frick@free.fr" target="_blank"
+                aria-label="Mail" />
+        </template>
+    </UFooter>
+</template>
