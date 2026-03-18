b042d257cda006e299b5c8970caecefc1440b5f3
Salomé Frick <salome.frick@free.fr>
2026-03-18 20:36:18 +0100

    update mobile menu

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -4,12 +4,16 @@ import type { NavigationMenuItem } from '@nuxt/ui'
 const items = <NavigationMenuItem[]>[
     { label: "Accueil", to: "/" },
     { label: "A Propos", to: "/about" },
-    { label: "Projets", to: "/projects" },]
+    { label: "Projets", to: "/projects" },
+]
 </script>
 
 <template>
     <UHeader title="" class="sticky">
         <UNavigationMenu :items="items" color="neutral" />
+        <template #body>
+            <UNavigationMenu :items="items" orientation="vertical" class="-mx-2.5" />
+        </template>
         <template #right>
             <UColorModeButton />
         </template>
