60059cb91f10ae7aaaed15c70c5e5690fc8e1073
Salomé Frick <salome.frick@free.fr>
2026-03-18 19:05:35 +0100

    add component header

---
+++ app/components/Header.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type { NavigationMenuItem } from '@nuxt/ui'
+
+const items = <NavigationMenuItem[]>[
+    { label: "Accueil", to: "/" },
+    { label: "A Propos", to: "/about" },
+    { label: "Projets", to: "/projects" },
+    { label: "Contact", to: "/contact" }
+]
+</script>
+
+<template>
+    <UHeader title="" class="sticky">
+        <UNavigationMenu :items="items" color="neutral" />
+        <template #right>
+            <UColorModeButton />
+        </template>
+    </UHeader>
+</template>
